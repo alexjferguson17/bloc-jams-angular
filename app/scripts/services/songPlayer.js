d5df0b3e90c925f4884a5d70c4347503a4517701
Alex Ferguson <alexjferguson17@gmail.com>
2016-02-22 15:12:03 -0500

    finishes up assignment 7

--- app/scripts/services/songPlayer.js
+++ app/scripts/services/songPlayer.js
@@ -24,13 +24,24 @@
            SongPlayer.currentSong = song;
         };
 
+
+        var playSong = function(song){
+          currentBuzzObject.play();
+          song.playing = true;
+        }
+
+        var stopSong = function(){
+          currentBuzzObject.stop();
+          SongPlayer.currentSong.playing = null;
+        }
+
         SongPlayer.play = function(song) {
           
           song = song || SongPlayer.currentSong;
           if (SongPlayer.currentSong !== song) {
           setSong(song);
-          currentBuzzObject.play();
-          song.playing = true;
+          playSong(song);
+          
           } else if (SongPlayer.currentSong === song) {
           if (currentBuzzObject.isPaused()) {
               currentBuzzObject.play();
@@ -45,18 +56,31 @@
     };
 
    SongPlayer.previous = function() {
-     var currentSongIndex = getSongIndex(SongPlayer.currentSong);
-     currentSongIndex--;
+       var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+       currentSongIndex--;
 
-     if (currentSongIndex < 0) {
-       currentBuzzObject.stop();
-       SongPlayer.currentSong.playing = null;
-     } else {
-         var song = currentAlbum.songs[currentSongIndex];
-         setSong(song);
-         playSong(song);
-     }
-  };
+       if (currentSongIndex < 0) {
+          stopSong();
+       } else {
+           var song = currentAlbum.songs[currentSongIndex];
+           setSong(song);
+           playSong(song);
+       }
+    };
+
+
+    SongPlayer.next = function() {
+       var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+       currentSongIndex++;
+
+       if (currentSongIndex > currentAlbum.songs.length) {
+          stopSong();
+       } else {
+           var song = currentAlbum.songs[currentSongIndex];
+           setSong(song);
+           playSong(song);
+       }
+    };
 
     return SongPlayer;
   }
